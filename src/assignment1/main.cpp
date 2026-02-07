6b13db6cf9119519090f2ed7753e5573b0d65dc0
RrazzmatazZ <496338815@qq.com>
2026-02-07 11:36:48 +0000

    assignment1 euler and Quaternion rotation control

--- src/assignment1/main.cpp
+++ src/assignment1/main.cpp
@@ -5,6 +5,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
 #include <iostream>
 
 #include "utils/Shader.h"
@@ -13,14 +17,16 @@
 #include "utils/Renderer.h"
 #include "utils/Skybox.h"
 
+#pragma region window and camera
 const unsigned int window_width = 1920;
 const unsigned int window_height = 1080;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 10.0f));
-float lastX = (float)window_width / 2.0;
-float lastY = (float)window_height / 2.0;
-bool firstMouse = true;
+#pragma endregion window and camera
 
+int rotationMode = 0;
+glm::vec3 eulerAngles = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::quat currentQuat = glm::identity<glm::quat>();
 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
@@ -37,23 +43,6 @@ std::string Path(const std::string& subPath)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-}
-
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
-    if (firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
-    camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 int main()
@@ -76,10 +65,7 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -88,6 +74,17 @@ int main()
     }
 
     glEnable(GL_DEPTH_TEST);
+
+#pragma region imgui init
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+#pragma endregion imgui
+
 
     Model aeroplane(RE("aeroplane.glb"), RE("aeroplane.vs"), RE("aeroplane.fs"));
 
@@ -114,29 +111,100 @@ int main()
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+#pragma region imgui panel
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+            ImGui::SetNextWindowSize(ImVec2(380, 280), ImGuiCond_Always);
+            ImGui::Begin("Aeroplane Control Panels");
+
+            ImGui::Text("Rotation Mode:");
+            ImGui::RadioButton("Euler", &rotationMode, 0);
+            ImGui::SameLine();
+            ImGui::RadioButton("Quaternion", &rotationMode, 1);
+
+            if (rotationMode == 0)
+            {
+                ImGui::Text("Euler Panel");
+                ImGui::SliderFloat("Pitch (X)", &eulerAngles.x, -90.0f, 90.0f);
+                ImGui::SliderFloat("Yaw   (Y)", &eulerAngles.y, -180.0f, 180.0f);
+                ImGui::SliderFloat("Roll  (Z)", &eulerAngles.z, -180.0f, 180.0f);
+            }
+
+            if (rotationMode == 1)
+            {
+                ImGui::Text("Quaternion Panel");
+                float rotSpeed = 500.0f * deltaTime;
+
+                glm::quat qPitch = glm::angleAxis(glm::radians(rotSpeed), glm::vec3(1, 0, 0));
+                glm::quat qYaw = glm::angleAxis(glm::radians(rotSpeed), glm::vec3(0, 1, 0));
+                glm::quat qRoll = glm::angleAxis(glm::radians(rotSpeed), glm::vec3(0, 0, 1));
+
+                ImGui::Text("Pitch (X):");
+                ImGui::SameLine();
+                if (ImGui::Button("Up##P")) currentQuat = currentQuat * qPitch;
+                ImGui::SameLine();
+                if (ImGui::Button("Down##P")) currentQuat = currentQuat * glm::inverse(qPitch);
+                ImGui::Text("Yaw   (Y):");
+                ImGui::SameLine();
+                if (ImGui::Button("Left##Y")) currentQuat = currentQuat * qYaw;
+                ImGui::SameLine();
+                if (ImGui::Button("Right##Y")) currentQuat = currentQuat * glm::inverse(qYaw);
+                ImGui::Text("Roll  (Z):");
+                ImGui::SameLine();
+                if (ImGui::Button("CCW##R")) currentQuat = currentQuat * qRoll;
+                ImGui::SameLine();
+                if (ImGui::Button("CW##R")) currentQuat = currentQuat * glm::inverse(qRoll);
+            }
+            if (ImGui::Button("Reset Orientation"))
+            {
+                eulerAngles = glm::vec3(0.0f, 0.0f, 0.0f);
+                currentQuat = glm::identity<glm::quat>();
+            }
+            ImGui::NewLine();
+
+            ImGui::Text("Fly Simulation Panel");
+            ImGui::End();
+        }
+#pragma endregion
+
         Renderer::BeginScene(camera, (float)window_width / (float)window_height);
         Renderer::SetSkybox(skybox);
         {
+            glm::mat4 eulerMatrix = glm::mat4(1.0f);
 
+            //yxz order euler
+            eulerMatrix = glm::rotate(eulerMatrix, glm::radians(eulerAngles.y), glm::vec3(0.0f, 1.0f, 0.0f));
+            eulerMatrix = glm::rotate(eulerMatrix, glm::radians(eulerAngles.x), glm::vec3(1.0f, 0.0f, 0.0f));
+            eulerMatrix = glm::rotate(eulerMatrix, glm::radians(eulerAngles.z), glm::vec3(0.0f, 0.0f, 1.0f));
 
+            glm::mat4 quatMatrix = glm::mat4_cast(glm::normalize(currentQuat));
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(.0f, 1.0f, 0.0f));
-            model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, .0f, 0.0f));
-            model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+            glm::mat4 baseMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+            baseMatrix = glm::rotate(baseMatrix, glm::radians(-90.0f), glm::vec3(.0f, 1.0f, 0.0f));
+            baseMatrix = glm::rotate(baseMatrix, glm::radians(-90.0f), glm::vec3(1.0f, .0f, 0.0f));
+            baseMatrix = glm::scale(baseMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
 
+            baseMatrix = eulerMatrix * quatMatrix * baseMatrix;
 
-            Renderer::Submit(aeroplane, model, [&](Shader* s)
+            Renderer::Submit(aeroplane, baseMatrix, [&](Shader* s)
             {
             });
         }
         Renderer::EndScene();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     Renderer::Shutdown();
-
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
